18273e4417db7b81a86fcce36dba9391d92a4677
Leon He <lhe868@gmail.com>
2019-08-16 11:49:26 +0800

    add print note title and content page

--- KindleNote.go
+++ KindleNote.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"regexp"
 	"net/http"
+	"net/url"
 )
 
 type Page struct{
@@ -26,11 +27,31 @@ type Notes struct{
 var data map[string][]Note;
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	page_content:=""
+	for k,_:=range data {
+		title,_:=url.QueryUnescape(k)
+		page_content+="<h1><a href='/note/"+title+"'>"+title+"</a></h1>"
+		
+	}
+    fmt.Fprintf(w, page_content, r.URL.Path[1:])
+}
+
+func bookHandler(w http.ResponseWriter,r *http.Request){
+	params:=(r.URL.Path[len("/note/"):])
+	notes,ok:=data[params]
+	content:= "<p><a href='/'>Back Home</a></p>"
+	if !ok {
+		fmt.Fprintf(w,content+"<p>Not Found:%q</p>",params)
+		return
+	}
+	for _,v:=range notes{
+		content+="<p>"+v.CreateTime+"</p><p>"+v.Content+"</p>"
+	}
+	fmt.Fprintf(w,content)
 }
 
 func main(){
-	content,err := ioutil.ReadFile("/Users/heyuanfei/Documents/MyClippings.txt")
+	content,err := ioutil.ReadFile("/Users/heyuanfei/Documents/MyClipping.txt")
 	if err !=nil {
 		log.Fatal(err)
 	}
@@ -40,6 +61,8 @@ func main(){
 	dateTimeRe:=regexp.MustCompile(` 添加于 (.*)\r\n`)
 	contentRe:=regexp.MustCompile(`\r\n(.*)\r\n={10}`)
 	contentReplaceRe:=regexp.MustCompile(`\r\n={10}`)
+	titleAndAuthor:=regexp.MustCompile(`[\s\S]*\s\(`)
+	titleReplaceWorld:=regexp.MustCompile(`[\s\(].*`)
 	a:=re.FindAll(content,-1)
 	count:=len(a)
 	if count>0 {
@@ -48,19 +71,25 @@ func main(){
 	for _,sourceContent:= range a {
 		//		fmt.Printf("d---%s",content) 
 		title:=titmeRe.Find(sourceContent)
-		//fmt.Printf("Title:%s\n",title)
+		title=titleAndAuthor.Find(title)
+		title=titleReplaceWorld.ReplaceAll(title, []byte{})
+		// fmt.Printf("Title:%s\n",title)
 		dateTime:=dateTimeRe.Find(sourceContent)
 		// fmt.Printf("Date:%s\n",dateTime)
 		findContent:=contentRe.Find(sourceContent)
 		content:=contentReplaceRe.ReplaceAll(findContent,[]byte{} )
 		// fmt.Printf("Content:%s\n",content)
-		
-		notes:=data[string(title)]
-		fmt.Println(notes)
+
+		var key string=string(title)//url.QueryEscape(string(title))
+		_,ok:=data[key]
+		if ok==false {
+			data[key] = []Note{}
+		}
 		note := Note{string(title),string(dateTime),string(content)}
-		data[string(title)]=append(notes,note)
+		 data[key]=append(data[key],note)
 	}
 	
-	// http.HandleFunc("/", handler)
-	// log.Fatal(http.ListenAndServe(":3001", nil))
+	 http.HandleFunc("/", handler)
+	 http.HandleFunc("/note/", bookHandler)
+	 log.Fatal(http.ListenAndServe(":3002", nil))
 }
